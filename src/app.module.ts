5ee6213a468242759c32485f49a60a15d4c2a36f
Adnan Mahmić <mahmica@gmail.com>
2022-03-03 16:43:26 +0100

    Implement the mechanism to make orders

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,7 @@ import { FeatureService } from './services/feature/feature.service';
 import { PhotoService } from './services/photo/photo.service';
 import { UserService } from './services/user/user.service';
 import { UserCartController } from './controllers/api/user.cart.controller';
+import { OrderService } from './services/order/order.servise';
 
 DatabaseConfiguration
 
@@ -85,6 +86,8 @@ DatabaseConfiguration
     FeatureService,
     UserService,
     CartService,
+    OrderService,
+    
 
 
   ],
