e65e88800ad9846ec74e6d48fcae0f4a592818a8
Adnan Mahmić <mahmica@gmail.com>
2022-03-22 17:20:36 +0100

    Fixes auth controller

--- src/controllers/api/auth.controller.ts
+++ src/controllers/api/auth.controller.ts
@@ -1,10 +1,12 @@
+const jwt = require("jsonwebtoken")
+
 import { Controller, Post, Body, Req, Put, HttpStatus, HttpException } from "@nestjs/common";
 import { AdministratorService } from "src/services/administrator/administrator.service";
 import { LoginAdministratorDto } from "src/dtos/administrator/login.administrator.dto";
 import { ApiResponse } from "src/misc/api.response.class";
 import * as crypto from 'crypto';
 import { LoginInfoDto } from "src/dtos/auth/login.info.dto";
-const jwt = require("jsonwebtoken")
+
 import { JwtDataDto } from "src/dtos/auth/jwt.data.dto";
 import { Request } from "express";
 import { jwtSecret } from "config/jwt.secret";
@@ -13,7 +15,7 @@ import { UserService } from "src/services/user/user.service";
 import { LoginUserDto } from "src/dtos/user/login.user.dto";
 import { JwtRefreshDataDto } from "src/dtos/auth/jwt.refresh.dto";
 import { UserRefreshTokenDto } from "src/dtos/auth/user.refresh.token.dto";
-
+import { AdministratorRefreshTokenDto } from "src/dtos/auth/administrator.refresh.token.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -27,7 +29,7 @@ export class AuthController {
         const administrator = await this.administratorService.getByUsername(data.username);
 
         if (!administrator) {
-            return new Promise(resolve => resolve(new ApiResponse('error', -3001, "Administrator is not exist!")));
+            return new Promise(resolve => resolve(new ApiResponse('error', -3001,"")));
         }
 
         const passwordHash = crypto.createHash('sha512');
@@ -35,7 +37,7 @@ export class AuthController {
         const passwordHashString = passwordHash.digest('hex').toUpperCase();
 
         if (administrator.passwordHash !== passwordHashString) {
-            return new Promise(resolve => resolve(new ApiResponse('error', -3002, "Wrong password!")));
+            return new Promise(resolve => resolve(new ApiResponse('error', -3002,"")));
         }
 
         const jwtData = new JwtDataDto();
@@ -66,7 +68,73 @@ export class AuthController {
             this.getIsoDate(jwtRefreshData.exp),
         );
 
+        await this.administratorService.addToken(
+            administrator.administratorId,
+            refreshToken,
+            this.getDatabseDateFormat(this.getIsoDate(jwtRefreshData.exp))
+        );
+
         return new Promise(resolve => resolve(responseObject));
+    }
+
+    @Post('administrator/refresh') // http://localhost:3000/auth/administrator/refresh/
+    async administratorTokenRefresh(@Req() req: Request, @Body() data: AdministratorRefreshTokenDto): Promise<LoginInfoDto | ApiResponse> {
+        const administratorToken = await this.administratorService.getAdministratorToken(data.token);
+
+        if (!administratorToken) {
+            return new ApiResponse("error", -10002, "No such refresh token!");
+        }
+
+        if (administratorToken.isValid === 0) {
+            return new ApiResponse("error", -10003, "The token is no longer valid!");
+        }
+
+        const sada = new Date();
+        const datumIsteka = new Date(administratorToken.expiresAt);
+
+        if (datumIsteka.getTime() < sada.getTime()) {
+            return new ApiResponse("error", -10004, "The token has expired!");
+        }
+
+        let jwtRefreshData: JwtRefreshDataDto;
+        
+        try {
+            jwtRefreshData = jwt.verify(data.token, jwtSecret);
+        } catch (e) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if (!jwtRefreshData) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if (jwtRefreshData.ip !== req.ip.toString()) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if (jwtRefreshData.ua !== req.headers["user-agent"]) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        const jwtData = new JwtDataDto();
+        jwtData.role = jwtRefreshData.role;
+        jwtData.id = jwtRefreshData.id;
+        jwtData.identity = jwtRefreshData.identity;
+        jwtData.exp = this.getDatePlus(60 * 5);
+        jwtData.ip = jwtRefreshData.ip;
+        jwtData.ua = jwtRefreshData.ua;
+
+        let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret);
+
+        const responseObject = new LoginInfoDto(
+            jwtData.id,
+            jwtData.identity,
+            token,
+            data.token,
+            this.getIsoDate(jwtRefreshData.exp),
+        );
+
+        return responseObject;
     }
 
     @Post('user/register') // POST http://localhost:3000/auth/user/register/
@@ -79,7 +147,7 @@ export class AuthController {
         const user = await this.userService.getByEmail(data.email);
 
         if (!user) {
-            return new Promise(resolve => resolve(new ApiResponse('error', -3001, "User is not exist")));
+            return new Promise(resolve => resolve(new ApiResponse('error', -3001,"")));
         }
 
         const passwordHash = crypto.createHash('sha512');
@@ -87,7 +155,7 @@ export class AuthController {
         const passwordHashString = passwordHash.digest('hex').toUpperCase();
 
         if (user.passwordHash !== passwordHashString) {
-            return new Promise(resolve => resolve(new ApiResponse('error', -3002, "Wrong password!")));
+            return new Promise(resolve => resolve(new ApiResponse('error', -3002,"")));
         }
 
         const jwtData = new JwtDataDto();
@@ -147,8 +215,7 @@ export class AuthController {
         }
 
         let jwtRefreshData: JwtRefreshDataDto;
-
-
+        
         try {
             jwtRefreshData = jwt.verify(data.token, jwtSecret);
         } catch (e) {
@@ -202,4 +269,3 @@ export class AuthController {
         return isoFormat.substr(0, 19).replace('T', ' ');
     }
 }
-
