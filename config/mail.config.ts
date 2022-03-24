1dfe9ca2eb1147f62c85ca905e75938c21aa2be9
Adnan Mahmić <mahmica@gmail.com>
2022-03-24 14:43:06 +0100

    added gitignore

--- config/mail.config.ts
+++
@@ -1,8 +0,0 @@
-export const MailConfig = {
-    hostname: 'smtp.gmail.com',
-    username: 'ado.dado10',
-    password: 'vaspassword',
-    senderEmail: 'ado.dado10@gmail.com',
-
-    orderNotificationMail: 'mahmica@gmail.com',
-}
